7db30d42e632a33d2f3753e2b59664fd86369af8
Oleg <o.lyagutov@gmail.com>
2022-08-10 23:01:24 +0300

    add zadacha 15

--- Program.cs
+++ Program.cs
@@ -11,3 +11,40 @@
 // int secondDigit = firstDigit % 10;
 // Console.WriteLine($"Вторая цифра: {secondDigit} ");
 
+// Задача 13: Напишите программу, которая выводит третью 
+// цифру заданного числа или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+
+//Console.Write("Введите число: ");
+// string number = Console.ReadLine();
+
+//int number = Convert.ToInt32(Console.ReadLine());
+//string IndexNumber = Console.ReadLine();
+//Console.WriteLine(IndexNumber[2]);
+
+// if ( IndexNumber < 2)
+// {
+//     Console.WriteLine("нет такой цифры");
+// }
+
+
+// Задача 15: Напишите программу, которая принимает 
+// на вход цифру, обозначающую день недели, и проверяет, 
+// является ли этот день выходным.
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+// Console.WriteLine("Введите число дня недели: ");
+// int DayNumber = Convert.ToInt32(Console.ReadLine());
+// if (DayNumber < 6)
+// {
+//     Console.WriteLine("Нет, не выходной. ");
+// }
+// else
+// {
+//     Console.WriteLine("Да, выходной. ");
+// }
